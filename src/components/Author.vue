6a75a5fda623b7b735509eac813f99237d5cffd3
Simone Calabrese <Simoneca3@gmail.com>
2021-07-01 18:22:07 +0200

    Authors

---
+++ src/components/Author.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+  <div class="author"><a href="author.html" class="author__cover author__cover--bg" data-bg="img/bg/bg-small.png"
+      style="background: url(&quot;img/bg/bg-small.png&quot;) center center / cover no-repeat;"></a>
+    <div class="author__meta"><a href="author.html" class="author__avatar author__avatar--verified"><img
+          src="img/avatars/avatar.jpg" alt=""></a>
+      <h3 class="author__name"><a href="author.html">Adam Zapel</a></h3>
+      <h3 class="author__nickname"><a href="author.html">@aaarthur</a></h3>
+      <p class="author__text">All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as
+        necessary</p>
+      <div class="author__wrap">
+        <div class="author__followers">
+          <p>3829</p><span>Followers</span>
+        </div><button class="author__follow" type="button">Follow</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name:"Author"
+}
+</script>
