5eec4dd18ed937df92229b021958514d557624cd
turinglabs <sebastiano.cataudo@gmail.com>
2021-07-23 11:40:53 +0200

    removing comments

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -4,9 +4,7 @@
     <Breadcrumb :links="[{link:'/', name:'Home', active:false}, {link:'/article/'+article._id, name:'Article', active:true}]"></Breadcrumb>
     <ArticleBlog :article="article"></ArticleBlog>
     <ArticleSidebar v-if="false"></ArticleSidebar>
-    <div class="col-12 col-xl-12">
-      <Comments></Comments>
-    </div>
+    
   </div>
 </div>
 </template>
@@ -15,7 +13,6 @@
 import Breadcrumb from '@/components/Breadcrumb.vue'
 import ArticleBlog from '@/components/ArticleBlog.vue'
 import ArticleSidebar from '@/components/ArticleSidebar.vue'
-import Comments from '../components/Comments.vue'
 
 import axios from 'axios'
 
@@ -24,8 +21,7 @@ export default {
   components: {
     Breadcrumb,
     ArticleBlog,
-    ArticleSidebar,
-    Comments
+    ArticleSidebar
   },
   watch: {
     '$route' (to) {
